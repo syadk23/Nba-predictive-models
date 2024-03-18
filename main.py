374107962ac8f22ae6b6a3486951b763960d64de
Syad K <syadkhwajazada@gmail.com>
2024-03-18 19:43:16 -0400

    add folder structure and .csv datasets

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from nba_api.stats.endpoints import leaguedashplayerstats
 import pandas as pd
+import pprint
 
 def get_player_stats(season):
     player_stats = leaguedashplayerstats.LeagueDashPlayerStats(season=season, per_mode_detailed='PerGame')
     stats = player_stats.get_data_frames()[0]
     return stats
+
+def get_player_advanced_stats(season):
+    player_advanced_stats = leaguedashplayerstats.LeagueDashPlayerStats(season=season, measure_type_detailed_defense='Advanced', per_mode_detailed='PerGame')
+    advanced_stats = player_advanced_stats.get_data_frames()[0]
+    return advanced_stats
 
 def get_player_avg_stats(stats, player_name=None):
     player_avg_stats = stats[['PLAYER_NAME', 'GP', 'W', 'L', 'W_PCT', 'MIN', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'TOV', 'PLUS_MINUS']]
@@ -13,6 +19,7 @@ def get_player_avg_stats(stats, player_name=None):
     return player_avg_stats
 
 def get_mvp_winners():
+    # Use last 10 as testing for now
     mvp_winners = {
         '2022-23': 'Joel Embiid',
         '2021-22': 'Nikola Jokic',
@@ -33,24 +40,27 @@ def main():
     end_year = 2023
     seasons = [f"{year}-{str(year+1)[-2:]}" for year in range(start_year, end_year + 1)]
 
-    all_stats = {}
+    all_basic_stats = {}
+    all_advanced_stats = {}
+    mvp_stats = {}
     for season in seasons:
-        all_stats[season] = get_player_stats(season)
+        all_basic_stats[season] = get_player_stats(season)
+        #all_advanced_stats[season] = get_player_advanced_stats(season)
 
+    #print(all_advanced_stats['2022-23'])
     mvp_winners = get_mvp_winners()
 
     for year, winner in mvp_winners.items():
-        if winner in all_stats[year]['PLAYER_NAME'].values:
-            mvp_stats = get_player_avg_stats(all_stats[year], winner)
-            print(mvp_stats)
+        if winner in all_basic_stats[year]['PLAYER_NAME'].values:
+            mvp_stats[year] = get_player_avg_stats(all_basic_stats[year], winner)
         else:
             print(winner, "stats were not found for the year", year)
 
-    """ player_stats = get_player_stats(seasons[-1])
+    pprint.pprint(mvp_stats)
+    player_stats = get_player_stats(seasons[-1])
 
-    #pd.set_option('display.max_columns', None)
-    player_avg_stats = get_player_avg_stats(player_stats).sort_values(by='PTS', ascending = False)
-    print(player_avg_stats) """
+    #player_avg_stats = get_player_avg_stats(player_stats).sort_values(by='PTS', ascending = False)
+    #print(player_avg_stats)
 
 if __name__ == "__main__":
     main()
